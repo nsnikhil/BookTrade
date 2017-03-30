6e7af88bd644f3060786be03105bc53a4bce20ca
nsnikhil <n.nikhil.ns65@gmail.com>
2017-03-30 15:36:52 +0530

    justimage

--- app/src/main/java/com/trade/book/booktrade/AddBook.java
+++ app/src/main/java/com/trade/book/booktrade/AddBook.java
@@ -560,8 +560,7 @@ public class AddBook extends AppCompatActivity implements View.OnClickListener {
                 try {
                     is = getContentResolver().openInputStream(data.getData());
                     Bitmap b = BitmapFactory.decodeStream(is);
-                    b.compress(Bitmap.CompressFormat.JPEG,50,null);
-                    imageList.add(b);
+                    imageList.add(getResizedBitmap(b,700));
                     imageContainer.swapAdapter(imageAdapter, true);
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
@@ -576,6 +575,21 @@ public class AddBook extends AppCompatActivity implements View.OnClickListener {
                 imageContainer.swapAdapter(imageAdapter, true);
             }
         }
+    }
+
+    public Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = (float)width / (float) height;
+        if (bitmapRatio > 1) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+        return Bitmap.createScaledBitmap(image, width, height, true);
     }
 
 
